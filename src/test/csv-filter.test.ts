7df6ffcb60cf173f33de1755de8fc09d9215b671
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-09-04 13:31:18 +0100

    🧪 inicial test

--- src/test/csv-filter.test.ts
+++ src/test/csv-filter.test.ts
@@ -1,3 +1,46 @@
-describe('CSV Filter', () => {
+import {CsvFilter} from "../core/csv-filter";
 
-})
+describe('CSV Filter', () => {
+    const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+    it('allow only correct lines', () => {
+        const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
+        const csvFilter = CsvFilter.create([header, invoiceLine]);
+        const result = csvFilter.filteredLines;
+
+        expect(result).toEqual([header, invoiceLine]);
+    });
+
+    it('excludes lines with both tax fields are exclusive', () => {
+        const invoiceLine = '1,02/05/2021,1000,790,21,7,ACER Laptop,B76430134,';
+        const csvFilter = CsvFilter.create([header, invoiceLine]);
+        const result = csvFilter.filteredLines;
+
+        expect(result).toEqual([header]);
+    });
+
+    it('excludes lines with both tax fields are empty', () => {
+        const invoiceLine = '1,02/05/2021,1000,790,,,ACER Laptop,B76430134,';
+        const csvFilter = CsvFilter.create([header, invoiceLine]);
+        const result = csvFilter.filteredLines;
+
+        expect(result).toEqual([header]);
+    });
+
+    it('excludes lines with non decimal tax fields', () => {
+        const invoiceLine = '1,02/05/2021,1000,790,XYZ,,ACER Laptop,B76430134,';
+        const csvFilter = CsvFilter.create([header, invoiceLine]);
+        const result = csvFilter.filteredLines;
+
+        expect(result).toEqual([header]);
+    });
+
+    it('excludes lines with both tax fields populated even if non decimal', () => {
+        const invoiceLine = '1,02/05/2021,1000,790,XYZ,7,ACER Laptop,B76430134,';
+        const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+        const result = csvFilter.filteredLines;
+
+        expect(result).toEqual([header]);
+    });
+
+});
